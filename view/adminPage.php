59d4270133e406a061fb87fab1446178202827bb
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-10-27 15:07:21 +0100

    ajout adminPage

---
+++ view/adminPage.php
@@ -0,0 +1,4 @@
+
+
+<a class="button" href="index.php?objet=user&action=alertsUser">Voir les nouveaux utilisateurs</a>
+<a class="button" href="index.php?objet=comment&action=alertsComment">Voir les commentaires signalés</a>

67785baa25875cc3578ba9568e69a4fac8aca012
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-10-29 12:16:40 +0100

    modif du header et adminPage selon les droits user

--- view/adminPage.php
+++ view/adminPage.php
@@ -1,10 +1,9 @@
-
 <?php if (array_key_exists('userLogin', $_SESSION) && $_SESSION['userLevel'] == 'admin') { ?>
     <a class="button" href="index.php?objet=user&action=alertsUser">Voir les nouveaux utilisateurs</a>
     <a class="button" href="index.php?objet=comment&action=alertsComment">Voir les commentaires signalés</a>
-    <?php 
-    } else {
-        header("Location: index.php?action=home");
-        exit();
-    } 
- ?>
+<?php
+} else {
+    header("Location: index.php?action=home");
+    exit();
+}
+?>
--- view/header.php
+++ view/header.php
@@ -3,11 +3,17 @@
 
 		<ul id="menu">
 			<li><a href="index.php?action=home">Accueil</a></li>
-				<li><a href='index.php?objet=user&action=create'>S'enregistrer</a></li>
-				<a href='index.php?objet=user&action=login'>S'identifier</a>
+			<li><a href='index.php?objet=user&action=create'>S'enregistrer</a></li>
+			<a href='index.php?objet=user&action=login'>S'identifier</a>
+			<?php if (array_key_exists('userLogin', $_SESSION)) { ?>
 				<a href='index.php?objet=post&action=postsList'>Archives APOD</a>
+			<?php } ?>
+			<?php if (array_key_exists('userLogin', $_SESSION) && $_SESSION['userLevel'] == 'admin') { ?>
 				<a href='index.php?objet=user&action=admin'>Admin</a>
+			<?php } ?>
+			<?php if (array_key_exists('userLogin', $_SESSION)) { ?>
 				<a href="index.php?objet=user&action=logout">Logout</a>
+			<?php } ?>
 		</ul>
 	</nav>
 </header>

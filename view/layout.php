36c4d241e48b536e8c0a60f57fc8d5e5ec60e4f4
Jerome Rouxel <celtic-jr@hotmail.fr>
2020-11-03 17:53:23 +0100

    ajout des balises dans le head

--- view/layout.php
+++ view/layout.php
@@ -3,8 +3,17 @@
 
 <head>
     <title>ExoWeb</title>
+    <meta name="description" content="Retrouver l'Apod du jour ainsi que les archives" />	
+	<meta property="og:locale" content="fr_FR" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Retrouver l'Apod du jour ainsi que les archives" />
+	<meta property="og:description" content="Retrouver l'Apod du jour ainsi que les archives" />
+	<meta property="og:url" content="https://www.jr-web.fr/projet5/public/index.php?ction=home" />
+	<meta property="og:site_name" content="Retrouver l'Apod du jour ainsi que les archives" />
+    <meta name="twitter:card" content="summary_large_image" />    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="canonical" href="https://www.jr-web.fr/projet5/public/index.php?ction=home" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css" />
 </head>
